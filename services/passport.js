10617142a3b7237b5b4a01d7114aea516dacf05c
kinkorea <alexkarasik89@gmail.com>
2017-09-06 13:20:48 -0400

    fixing package.json issue

--- services/passport.js
+++ services/passport.js
@@ -18,12 +18,15 @@ passport.deserializeUser((id, done) => {
 //clientID 650778914974-99h8s6lhbv97u0k2de64m747i61q3gda.apps.googleusercontent.com
 //CLIENT SECRET 1A46sPzRyqi52FVreeUJnMXb
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy: true
-}, async(accessToken, refreshToken, profile, done) => {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    }, 
+async(accessToken, refreshToken, profile, done) => {
     const existingUser = await User.findOne({googleId: profile.id})
     if (existingUser) {
         //we already have a record with the give profileId
