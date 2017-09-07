b448ac6bf8e6d5fe76ebf83726393071131d3df5
kinkorea <alexkarasik89@gmail.com>
2017-09-07 13:58:26 -0400

    merging again to master to be safe

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(express.static('client/build'));
 const path = require('path');
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-})
+});
 
 }
 
